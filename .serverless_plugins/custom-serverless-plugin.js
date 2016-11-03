ee26735562068b2716999a39a51901f387e62800
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-11-04 00:49:16 +0900

    wip

---
+++ .serverless_plugins/custom-serverless-plugin.js
@@ -0,0 +1,47 @@
+'use strict';
+
+class CustomServerlessPlugin {
+  constructor() {
+    this.commands = {
+      deploy: {
+        lifecycleEvents: [
+          'resources',
+          'functions'
+        ]
+      },
+    };
+  }
+}
+
+//class Deploy {
+//  constructor() {
+//    this.commands = {
+//      deploy: {
+//        lifecycleEvents: [
+//          'resources',
+//          'functions'
+//        ]
+//      },
+//    };
+//
+//    this.hooks = {
+//      'before:deploy:resources': this.beforeDeployResources,
+//      'deploy:resources': this.deployResources,
+//      'after:deploy:functions': this.afterDeployFunctions
+//    };
+//  }
+//
+//  beforeDeployResources() {
+//    console.log('Before Deploy Resources');
+//  }
+//
+//  deployResources() {
+//    console.log('Deploy Resources');
+//  }
+//
+//  afterDeployFunctions() {
+//    console.log('After Deploy Functions');
+//  }
+//}
+
+module.exports = CustomServerlessPlugin
