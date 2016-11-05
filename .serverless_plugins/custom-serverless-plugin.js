6489fe47c21f5a440714587b242aa214a89bf0bb
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-11-05 12:03:36 +0900

    $ SLS_DEBUG=1 sls deplo

--- .serverless_plugins/custom-serverless-plugin.js
+++ .serverless_plugins/custom-serverless-plugin.js
@@ -1,47 +1,34 @@
 'use strict';
 
-class CustomServerlessPlugin {
+class Deplo {
   constructor() {
     this.commands = {
-      deploy: {
+      deplo: {
         lifecycleEvents: [
           'resources',
           'functions'
         ]
       },
     };
+
+    this.hooks = {
+      'before:deplo:resources': this.beforeDeployResources,
+      'deplo:resources': this.deployResources,
+      'after:deplo:functions': this.afterDeployFunctions
+    };
+  }
+
+  beforeDeployResources() {
+    console.log('Before Deploy Resources');
+  }
+
+  deployResources() {
+    console.log('Deploy Resources');
+  }
+
+  afterDeployFunctions() {
+    console.log('After Deploy Functions');
   }
 }
 
-//class Deploy {
-//  constructor() {
-//    this.commands = {
-//      deploy: {
-//        lifecycleEvents: [
-//          'resources',
-//          'functions'
-//        ]
-//      },
-//    };
-//
-//    this.hooks = {
-//      'before:deploy:resources': this.beforeDeployResources,
-//      'deploy:resources': this.deployResources,
-//      'after:deploy:functions': this.afterDeployFunctions
-//    };
-//  }
-//
-//  beforeDeployResources() {
-//    console.log('Before Deploy Resources');
-//  }
-//
-//  deployResources() {
-//    console.log('Deploy Resources');
-//  }
-//
-//  afterDeployFunctions() {
-//    console.log('After Deploy Functions');
-//  }
-//}
-
-module.exports = CustomServerlessPlugin
+module.exports = Deplo
